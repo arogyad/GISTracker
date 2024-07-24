ad48a54c6bae9b0213dd4a23b454c4d2dfd61998
arogya <arogyadahal2003@gmail.com>
2024-07-24 11:36:02 +0545

    Initial Commit

---
+++ GISTracker/GISTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GISTrackerApp.swift
+//  GISTracker
+//
+//  Created by Arogya Dahal on 7/24/24.
+//
+
+import SwiftUI
+
+@main
+struct GISTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GISTrackerTests/GISTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  GISTrackerTests.swift
+//  GISTrackerTests
+//
+//  Created by Arogya Dahal on 7/24/24.
+//
+
+import Testing
+@testable import GISTracker
+
+struct GISTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
